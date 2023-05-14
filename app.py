4e902ba7a3ad58f9b756f9797eb0b8b0bad057d4
winter_x64 <itswinterx64@gmail.com>
2023-05-14 22:54:56 +0530

    Initial commit
    Added the env config file and added main file 'app.py', 'index.html' for the home page

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    column1 = db.Column(db.String(50))
+    column2 = db.Column(db.String(50))
+    column3 = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f'<Data {self.id}>'
+
+@app.route('/')
+def index():
+    data = Data.query.all()
+    return render_template('index.html', data=data)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    df = pd.read_excel(file)
+    df.to_sql('data', con=db.engine, if_exists='append', index=False)
+    return 'File uploaded successfully'
+
+if __name__ == '__main__':
+    app.run(debug=True)
